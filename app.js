cdde65c38546db92e3da5e9983dc3d6116ea0fad
Chris Allen <chrishughallen@gmail.com>
2018-10-28 13:16:51 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+$("#courseBtn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#sub").offset().top
+    }, 1000);
+});
+
+
+$("#aboutBtn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 1000);
+});
+
+
+$("#contactBtn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contactForm").offset().top
+    }, 1000);
+});
+
+$("#mainContactBtn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contactForm").offset().top
+    }, 1000);
+});
